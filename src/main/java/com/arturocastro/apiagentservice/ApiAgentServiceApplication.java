482a492c29b04a76ebcd372dcc066e8cfe1d30af
AJoelCastro <misthorn33@gmail.com>
2025-11-11 23:10:21 -0500

    first commit

---
+++ src/main/java/com/arturocastro/apiagentservice/ApiAgentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.arturocastro.apiagentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAgentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAgentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arturocastro/apiagentservice/ApiAgentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturocastro.apiagentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAgentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
