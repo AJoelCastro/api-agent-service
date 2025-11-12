57c098a33377ec60e9c7f9a653d2dc6deeed35ef
AJoelCastro <misthorn33@gmail.com>
2025-11-11 23:15:17 -0500

    agent service and controller

---
+++ src/main/java/com/arturocastro/apiagentservice/controller/AgentControlller.java
@@ -0,0 +1,10 @@
+package com.arturocastro.apiagentservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/openai/agent")
+public class AgentControlller {
+
+}
