35e96f17a002158a3ba23399e21730776b681b44
AJoelCastro <misthorn33@gmail.com>
2025-11-12 17:20:10 -0500

    Agent service config 2

--- src/main/java/com/arturocastro/apiagentservice/controller/AgentControlller.java
+++ src/main/java/com/arturocastro/apiagentservice/controller/AgentControlller.java
@@ -1,10 +1,27 @@
 package com.arturocastro.apiagentservice.controller;
 
+import com.arturocastro.apiagentservice.service.AgentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/openai/agent")
 public class AgentControlller {
+
+    private final AgentService agentService;
+
+    public AgentControlller(AgentService agentService){
+        this.agentService = agentService;
+    }
+
+    @PostMapping("/test")
+    public ResponseEntity<?> agentBuilder(){
+        agentService.agentBuilder();
+        return ResponseEntity.ok().build();
+    }
 
 }
--- src/main/java/com/arturocastro/apiagentservice/service/AgentService.java
+++ src/main/java/com/arturocastro/apiagentservice/service/AgentService.java
@@ -2,6 +2,7 @@ package com.arturocastro.apiagentservice.service;
 
 import com.openai.client.OpenAIClient;
 import com.openai.client.okhttp.OpenAIOkHttpClient;
+import com.openai.models.beta.chatkit.ChatKitWorkflow;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,23 @@ public class AgentService {
         client = OpenAIOkHttpClient.builder()
                 .apiKey(apiKey)
                 .build();
+    }
+
+    public void agentBuilder(){
+        ChatKitWorkflow chatKitWorkflow = ChatKitWorkflow.builder()
+                .id("wf_6915055d87ec819094e860f95237ac3701c8ecc6713ba46d")
+                .stateVariables(
+                        ChatKitWorkflow.StateVariables.builder()
+                                .build()
+                )
+                .tracing(
+                        ChatKitWorkflow.Tracing.builder()
+                                .enabled(true)
+                                .build()
+                )
+                .version("1")
+                .build();
+        System.out.println(chatKitWorkflow);
     }
 
 }
