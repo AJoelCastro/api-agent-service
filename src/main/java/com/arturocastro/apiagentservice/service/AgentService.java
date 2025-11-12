9caea50de77ee4c61897779bcc5feb0b222f274b
AJoelCastro <misthorn33@gmail.com>
2025-11-12 10:28:45 -0500

    Agent service config

--- src/main/java/com/arturocastro/apiagentservice/service/AgentService.java
+++ src/main/java/com/arturocastro/apiagentservice/service/AgentService.java
@@ -1,8 +1,19 @@
 package com.arturocastro.apiagentservice.service;
 
+import com.openai.client.OpenAIClient;
+import com.openai.client.okhttp.OpenAIOkHttpClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AgentService {
+
+    private final OpenAIClient client;
+
+    public AgentService(@Value("${openai.api.key}") String apiKey){
+        client = OpenAIOkHttpClient.builder()
+                .apiKey(apiKey)
+                .build();
+    }
 
 }
