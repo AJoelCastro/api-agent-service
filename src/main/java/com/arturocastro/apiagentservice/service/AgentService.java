231507831359a29253cfce47cc066452cf7123cc
AJoelCastro <misthorn33@gmail.com>
2025-11-14 17:50:34 -0500

    Chat session fix 2

--- src/main/java/com/arturocastro/apiagentservice/service/AgentService.java
+++ src/main/java/com/arturocastro/apiagentservice/service/AgentService.java
@@ -2,19 +2,15 @@ package com.arturocastro.apiagentservice.service;
 
 import com.openai.client.OpenAIClient;
 import com.openai.client.okhttp.OpenAIOkHttpClient;
-import com.openai.core.JsonField;
 import com.openai.core.JsonValue;
 import com.openai.models.ChatModel;
 import com.openai.models.beta.assistants.Assistant;
 import com.openai.models.beta.assistants.AssistantCreateParams;
-import com.openai.models.beta.chatkit.ChatKitWorkflow;
 import com.openai.models.beta.chatkit.sessions.SessionCreateParams;
 import com.openai.models.beta.chatkit.threads.*;
-import com.openai.models.beta.threads.ThreadCreateAndRunParams;
 import com.openai.models.beta.threads.ThreadCreateParams;
 import com.openai.models.beta.threads.runs.Run;
 import com.openai.models.beta.threads.runs.RunCreateParams;
-import com.openai.services.blocking.beta.chatkit.SessionService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -72,46 +68,13 @@ public class AgentService {
 
         System.out.println("chatSession: " + chatSession);
 
-        AssistantCreateParams assistantCreateParams = AssistantCreateParams.builder()
-                .model(ChatModel.GPT_4O)
-                .instructions("Eres un excelente innovador que ha creado las mas grandes empresas del mundo")
-                .build();
-
-        Assistant assistant = client.beta().assistants().create(assistantCreateParams);
-
-        System.out.println("assistant: " + assistant);
-
-        ThreadCreateParams threadCreateParams = ThreadCreateParams.builder()
-                .build();
-
-        com.openai.models.beta.threads.Thread thread = client.beta().threads().create(threadCreateParams);
-
-        System.out.println("thread: " + thread);
-
-        RunCreateParams runCreateParams = RunCreateParams.builder()
-                .threadId(thread.id())
-                .assistantId(assistant.id())
-                .build();
-
-        Run run = client.beta().threads().runs().create(runCreateParams);
-
-        System.out.println("run: " + run);
-
-        ThreadRetrieveParams threadRetrieveParams = ThreadRetrieveParams.builder()
-                .threadId(run.threadId())
-                .build();
-
-        ChatKitThread chatKitThread = client.beta().chatkit().threads().retrieve(threadRetrieveParams);
-
-        System.out.println("chatKitThread: " + chatKitThread);
-
-
-//        2025-11-13T15:53:35.648-05:00 ERROR 37636 --- [api-agent-service] [nio-8080-exec-2] o.a.c.c.C.[.[.[/].[dispatcherServlet]    : Servlet.service() for servlet [dispatcherServlet] in context with path [] threw exception [Request processing failed: com.openai.errors.BadRequestException: 400: Invalid 'thread_id': 'thread_VfOIwMfibDwVjdzFHcn9AmQX'. Expected an ID that begins with 'cthr'.] with root cause
-//
-//com.openai.errors.BadRequestException: 400: Invalid 'thread_id': 'thread_VfOIwMfibDwVjdzFHcn9AmQX'. Expected an ID that begins with 'cthr'.
+////        2025-11-13T15:53:35.648-05:00 ERROR 37636 --- [api-agent-service] [nio-8080-exec-2] o.a.c.c.C.[.[.[/].[dispatcherServlet]    : Servlet.service() for servlet [dispatcherServlet] in context with path [] threw exception [Request processing failed: com.openai.errors.BadRequestException: 400: Invalid 'thread_id': 'thread_VfOIwMfibDwVjdzFHcn9AmQX'. Expected an ID that begins with 'cthr'.] with root cause
+////
+////com.openai.errors.BadRequestException: 400: Invalid 'thread_id': 'thread_VfOIwMfibDwVjdzFHcn9AmQX'. Expected an ID that begins with 'cthr'.
 
         return client.beta().chatkit().sessions().create(sessionCreateParams);
 
     }
+
 
 }
